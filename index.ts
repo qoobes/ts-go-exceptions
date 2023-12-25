dfec9de0123f8640fff79c46dc0230ef1e04d938
Ahmed Haracic <me@qoobes.space>
2023-12-25 14:10:58 +0100

    Better types

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export const handleException = <ResultType = unknown, ErrorType = unknown>(
   promise: Promise<ResultType>
-): Promise<[ResultType | null, ErrorType | null]> =>
-  promise
-    .then(data => [data, null] as [ResultType | null, ErrorType | null])
-    .catch(error => [null, error] as [ResultType | null, ErrorType | null])
+): Promise<[null, ErrorType] | [ResultType, null]> => promise
+    .then(data => [data, null] as [ResultType,  null])
+    .catch(error => [null, error] as [null, ErrorType])
+
